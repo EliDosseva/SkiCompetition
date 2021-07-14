5273705d49f25ca4bd7b5ba08987eda36c6d8bd9
EliDosseva <85696338+EliDosseva@users.noreply.github.com>
2021-07-14 14:33:58 +0300

    Delete MyExample/MyExample directory

--- MyExample/MyExample/dbo.StartList.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[StartList] (
-    [ID]        INT          CONSTRAINT [DF_StartList_ID] DEFAULT ((1)) NOT NULL,
-    [FirstName] VARCHAR (50) NOT NULL,
-    [Sex]       VARCHAR (20) NOT NULL,
-    [Team]      VARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_StartList] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
